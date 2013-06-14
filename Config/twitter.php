8110b0b9c2c661ee0a89e0a467a69872d6fce9bf
Rob Sawyer <robksawyer@gmail.com>
2013-06-14 16:52:01 -0700

    Added config

---
+++ Config/twitter.php
@@ -0,0 +1,9 @@
+<?php 
+$config = array(
+	'Twitter' => array(
+		'version' => '1.1',
+		'consumerKey' => getenv('TWITTER_CONSUMER_KEY'),
+		'consumerSecret' => getenv('TWITTER_CONSUMER_SECRET')
+	)
+);
+?>
